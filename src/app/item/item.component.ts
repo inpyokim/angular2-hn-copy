201b552c42e4fb2e3ade525fcd453f8c2450cd58
Byeong-Guk Min <maxmin93@gmail.com>
2017-02-13 00:13:31 +0900

    Angular2-Hn 따라하기

---
+++ src/app/item/item.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { HackerNewsAPIService } from '../hackernews-api.service';
+
+@Component({
+  selector: 'item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.scss']
+})
+export class ItemComponent implements OnInit {
+
+  @Input() itemID: number;
+  item;
+  
+  // constructor() { }
+  // ngOnInit() { }
+
+  constructor(private _hackerNewsAPIService: HackerNewsAPIService) { }
+
+  ngOnInit() {
+    this._hackerNewsAPIService.fetchItem(this.itemID)
+            .subscribe(
+              data => { 
+                this.item = data; 
+                console.log( `item[${this.itemID}] = "${this.item['title']}"` ); 
+              }, 
+              error => console.log('Could not load item' + this.itemID)
+            );
+  }
+
+}
---
+++ src/app/stories/stories.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { HackerNewsAPIService } from '../hackernews-api.service';
+
+@Component({
+  selector: 'app-stories',
+  templateUrl: './stories.component.html',
+  styleUrls: ['./stories.component.scss']
+})
+export class StoriesComponent implements OnInit {
+  items;  // : number[];
+
+  // constructor() { this.items = Array(30); }
+  constructor(private _hackerNewsAPIService: HackerNewsAPIService) {}
+
+  ngOnInit() {
+    this._hackerNewsAPIService
+            .fetchStories()
+            .subscribe(
+                items => this.items = items,
+                error => console.log('Error fetching stories')
+            );
+  }
+
+}
